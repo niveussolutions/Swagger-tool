e3e7dc88cfe2492cb65eccc05fa6a7745fe5173a
Karthik V Rao <karthik.rao@niveussolutions.com>
2023-03-29 18:36:17 +0530

    Now we can add the output path

--- pack/utility/utility.ts
+++ pack/utility/utility.ts
@@ -1,3 +1,6 @@
+import { extname,dirname } from "path";
+import * as fs from 'fs';
+
 function spaces(count:number):string{
     let spaces:string=""
     for(let i:number=1;i<=count;i++){
@@ -17,6 +20,22 @@ function findDataTypes(data){
         }
         return type
     }
-export {spaces,findDataTypes};
+ function verifyOutputPath(path:any){
+        if(path){
+    let extension = extname(path);
+       
+        if(extension){
+            if(extension!==".yaml"){
+                console.log("Output file path Extension is Wrong ⛔⛔");
+                return false
+            }
+        if(fs.existsSync(dirname(path))) 
+            return true  
+        
+        }}
+        console.log("Output file path is Wrong ⛔⛔");
+        return false
+        }
+export {spaces,findDataTypes,verifyOutputPath};
 
 // \n${space(spaces)}items :\n${space(spaces+1)}type :${findDataType(data[0])}
--- run.ts
+++ run.ts
@@ -1,9 +1,13 @@
 import * as fs from 'fs';
 import {  getSwaggerData } from './pack/getFullCollection';
+import { verifyOutputPath } from './pack/utility/utility';
 let path=process.argv
 fs.readFile(path[2],(error,data:any)=>{    
      try{
-   data=JSON.parse(data)
+//Checking that path is exist or not 
+if(fs.existsSync(path[2])){
+   
+data=JSON.parse(data)
 
 // ** handling extra Array
    let input:any
@@ -13,20 +17,33 @@ fs.readFile(path[2],(error,data:any)=>{
    }
    else
    input=data.item[0]
+   
+   let yamlFilePath='./output/output.yaml'
+//checking path for output yaml file 
+if(verifyOutputPath(path[3])){
 
+   yamlFilePath=path[3]
+}
 
 
 // ***
+
     
   //writing the file 
-  fs.writeFile('./output/output.yaml', getSwaggerData(data), () => {
+  fs.writeFile(yamlFilePath, getSwaggerData(data), () => {
     console.log("\n\n*******     done       *********\n");
+    console.log(`Output Path ${yamlFilePath}\n\n`);
+    
     // console.log("If response is not in your postman collection an example response will be added automatically\n");
     console.log("**********************----------------****************-------------------************************\n\n");
 });
+}
+else
+console.log("⛔⛔⛔   ERROR   ⛔ ⛔⛔ \nCollection path is not correct\n\n");
+
      }catch(e){
         //Printing the error
-      console.log("\n\n*****************************          ERROR             ******************************************\n"+e);
+      console.log("\n\n************************       ⛔⛔⛔   ERROR   ⛔ ⛔⛔         ********************************\n"+e);
       console.log("Collection is not correct\n\n");
      }
   })
